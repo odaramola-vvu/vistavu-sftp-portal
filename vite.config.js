be651ecb2c19286e87138fb30587bdd97c13fca3
Oluseyi Daramola <oluseyi.daramola@vistavusolutions.com>
2025-04-07 23:58:59 -0600

    Fix base path and successful Vite build

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/', // <--- Add this line
   plugins: [react()],
   define: {
     global: {},
